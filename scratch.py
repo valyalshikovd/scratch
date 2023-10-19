69ab28d563d2ba14f78ffd5fb827c28892e4ced8
valyalshikovd <770vda@mail.ru>
2023-10-19 16:48:49 +0300

    авторизую парсер

--- scratch.py
+++ scratch.py
@@ -1,2 +1,24 @@
 from bs4 import BeautifulSoup
+import fake_useragent
+import requests
 
+def scratch():
+    url = "https://www.cs.vsu.ru/brs/login"
+    data = {
+        'login': '---',
+        'password': '---',
+        'button_login': 'Вход'
+    }
+    user = fake_useragent.UserAgent.random
+    # header = {
+    #     'user-agent': user
+    # }
+    headers = {
+        "Accept": "*/*",
+        "User-Agent": " --- "
+    }
+   # request = requests.get(url, headers=header)
+    responce = requests.post(url, data=data, headers=headers).text
+    print(responce)
+    with open("index.html", "w", encoding="utf-8-sig") as file:
+        file.write(responce)
